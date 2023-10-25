6a569e780fecea821e6c93489094aceef1728634
ig-rudenko <ig.rudenko1@yandex.ru>
2023-10-25 21:29:27 +0300

    Создан тестовый файл для чтения картинки

---
+++ lesson9/file-image.py
@@ -0,0 +1,23 @@
+import pathlib
+
+# Открываем в бинарном формате файл (ЛЮБОЙ)
+# Размер файла
+print(pathlib.Path("img.png").stat().st_size)
+
+# Бинарный на чтение (НЕТ КОДИРОВКИ)
+with open("img.png", "br") as file:
+    image = file.read()
+    # Перевели байты в hex (16-ая сист. исч.)
+    print("Кол-во байт в картинке", len(image.hex()) // 2)
+
+
+new_bytes = b"Python\x00"
+print(new_bytes)
+
+# Символ сердечка
+
+h = "♥"
+bytes_h = h.encode(encoding="utf-8")
+
+print("♥ В байтах -", bytes_h)
+print("♥ В 16ричной (hex) -", bytes_h.hex())
