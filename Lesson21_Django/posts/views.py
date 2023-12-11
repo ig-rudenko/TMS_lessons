5f6b9bea24be131a6ec8a0052922f960b3e908f6
ig-rudenko <ig.rudenko1@yandex.ru>
2023-12-11 22:15:18 +0300

    Lesson 21 Django (urls views)

---
+++ Lesson21_Django/posts/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, Http404
+from django.core.handlers.wsgi import WSGIRequest
+
+from .models import Note
+
+
+def home_page_view(request: WSGIRequest):
+    # Обязательно! каждая функция view должна принимать первым параметром request.
+    all_notes = Note.objects.all()  # Получение всех записей из таблицы этой модели.
+    context: dict = {
+        "notes": all_notes
+    }
+    return render(request, "home.html", context)
+
+
+def create_note_view(request: WSGIRequest):
+    if request.method == "POST":
+        note = Note.objects.create(
+            title=request.POST["title"],
+            content=request.POST["content"],
+        )
+        return HttpResponseRedirect("/note/" + str(note.uuid))
+
+    # Вернется только, если метод не POST.
+    return render(request, "create_form.html")
+
+
+def show_note_view(request: WSGIRequest, note_uuid):
+    try:
+        note = Note.objects.get(uuid=note_uuid)  # Получение только ОДНОЙ записи.
+
+    except Note.DoesNotExist:
+        # Если не найдено такой записи.
+        raise Http404
+
+    return render(request, "note.html", {"note": note})
