2587c8b32d8d76b11edd15a7639cf255cf8dcd94
ig-rudenko <ig.rudenko1@yandex.ru>
2024-02-07 21:23:59 +0300

    Lesson 35 Celery beat Bugfix: send_email_task

--- food/app/tasks.py
+++ food/app/tasks.py
@@ -24,7 +24,7 @@ def send_periodical_email():
 
 @shared_task(ignore_result=True, max_retries=3, autoretry_for=(Exception,))
 def send_email_task(message: str, email: str, subject: str):
-    print("sending email", email, subject, message)
+    print(f"sending email `{email}`, `{subject}`, `{message}`")
 
     if not email or not message:
         return
