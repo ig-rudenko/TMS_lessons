2286b4585acc3c58eb41241d67b1e1a9adfcd2ea
ig-rudenko <ig.rudenko1@yandex.ru>
2024-03-25 22:26:07 +0300

    Lesson 48 Telegram Bot (aiogram)

--- food/users/admin.py
+++ food/users/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.admin import UserAdmin
 
 from users.models import User
@@ -6,4 +7,20 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
-    pass
+    fieldsets = (
+        (None, {"fields": ("username", "tg_id", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
--- food/users/models.py
+++ food/users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    tg_id = models.IntegerField(null=True, blank=True, unique=True, db_index=True)
