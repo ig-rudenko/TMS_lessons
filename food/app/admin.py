a78fa31ef72d77211abd92c355ba7f8cae9bd1d4
ig-rudenko <ig.rudenko1@yandex.ru>
2024-12-18 22:24:58 +0300

    update

--- food/app/admin.py
+++ food/app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.sessions.models import Session
 
+from users.models import User
 from .models import Ingredient, Recipe
 
 
@@ -12,9 +13,26 @@ class SessionAdmin(admin.ModelAdmin):
     list_display = ['session_key', '_session_data', 'expire_date']
 
 
+class IsMemberUserFilter(admin.SimpleListFilter):
+    title = 'Название фильтра'
+    parameter_name = 'is_member'
+
+    def lookups(self, request, model_admin):
+        qs = User.objects.filter(is_member=True)
+        return (
+            (u.username, u.username) for u in qs
+        )
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(user__username=self.value())
+        return queryset
+
+
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["name", "user"]
+    list_filter = [IsMemberUserFilter]
 
 
 @admin.register(Ingredient)
--- food/run.sh
+++ food/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-poetry run python manage.py createsuperuser --noinput;
 poetry run python manage.py migrate;
+poetry run python manage.py createsuperuser --noinput;
 poetry run gunicorn -w 2 -b 0.0.0.0:8000 food.wsgi:application;
